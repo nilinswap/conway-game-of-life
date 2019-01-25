5c03aefb059dceb801736447f9517f2ea2810033
nilinswap <nilinswap@gmail.com>
2019-01-25 16:14:56 +0530

    pre-commit hook test

--- dummy_index.js
+++ dummy_index.js
@@ -1,32 +1,4 @@
-/*
-import * as wasm from "wasm-game-of-life";
 
-
-//wasm.univ();
-function sleep(delay) {
-        var start = new Date().getTime();
-        while (new Date().getTime() < start + delay);
-      }
-
-//alert('swapnil');
-var element = document.querySelector('body');
-//console.log(element.innerHTML);
-var univ = wasm.get_universe();
-function game_loop(){
-
-    element.innerText = univ.render();
-    univ.tick();
-    sleep(1000);
-    debugger;
-    //console.log(univ.total_living);
-    requestAnimationFrame(game_loop);
-
-}
-
-game_loop();
-
-
-*/
 
 
 import { Universe, Cell } from "wasm-game-of-life";
